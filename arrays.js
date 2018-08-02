4562b884de6f477d644dcbb7e1e172cb59f5340d
Jess Guttenberg <jessigu@gmail.com>
2018-08-02 15:54:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,3 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-var superheroines = ["catwoman", "she-hulk", "mystique"];
- 
-superheroines.push("wonder woman");
 
-console.log(superheroines[3])
